ac9f33db88f1e1b343677bb6ca2adb1e4b0f2560
Nikitha Keerthana <58261586+nikki-ofcl@users.noreply.github.com>
2026-06-03 19:08:36 +0530

    Delete 1.py

--- 1.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        sums = {}
-        for i, x in enumerate(nums):
-            if (y := target - x) in sums:
-                return [sums[y], i]
-            sums[x] = i
